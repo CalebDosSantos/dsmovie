6c59f630752eb8d73fb67b276e964bb98aea7956
caleb-santos <caleb.santos.dev@gmail.com>
2022-01-15 19:57:16 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,43 +9,51 @@ function Listing() {
 
   const [pageNumber, setPageNumber] = useState(0);
 
-  useEffect(() => {
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
-  .then(response => {
-    const data = response.data as MoviePage;
-    setPageNumber(data.number);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true
   });
 
-  }, []);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+    .then(response => {
+      const data = response.data as MoviePage;
+      setPage(data);
+    });
+  }, [pageNumber]);
 
+  // const movie = {
+    // id: 1,
+    // image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    // title: "The Witcher",
+    // count: 2,
+    // score: 4.5
+// };
 
-  return (
-    <>
-    <p>{pageNumber}</p>
+return (
+  <>
   <Pagination />
 
   <div className="container">
     <div className="row">
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-    </div>
-  </div>
+      {page.content.map(movie => (
 
-  <MovieCard />
-</>);
+        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <MovieCard movie={movie} />
+        </div>
+      )
+                       )}
+
+    </div>
+    </div>
+    </>);
 }
 
 export default Listing;
